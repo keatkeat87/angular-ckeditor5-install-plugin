c90e968e433adb93947561c7598c3055cfcb538b
keatkeat87 <keatkeat87@live.com>
2018-05-15 17:00:16 +0800

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic'; it work 
+import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 
@@ -9,9 +9,21 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  start() {
+
+  buildClassic() {
+    ClassicEditorBuild
+      .create(document.querySelector('#build-classic'))
+      .then(editor => {
+        console.log('Editor was initialized', editor);
+      })
+      .catch(error => {
+        console.error(error.stack);
+      });
+  }
+
+  editorClassic() {
     ClassicEditor
-      .create(document.querySelector('#editor'), {
+      .create(document.querySelector('#editor-classic'), {
         plugins: [Bold],
         toolbar: ['bold']
       })
